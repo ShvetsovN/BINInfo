05e109a7592210955e14a7c99a70094d2f808fdb
= <=>
2025-02-24 17:30:57 +0500

    BI-2: Настройка макета первого экрана 1. Настроен первый экран 2. Добавлены data class

--- app/src/main/java/com/example/bininfo/MainActivity.kt
+++ app/src/main/java/com/example/bininfo/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bininfo.ui.theme.BINInfoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +23,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             BINInfoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                            .padding(horizontal = 10.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+
+                        CheckCardField()
+                        Spacer(modifier = Modifier.height(4.dp))
+                        FieldDescription()
+                        LookUpButton()
+                        Spacer(modifier = Modifier.height(4.dp))
+                        CardInfo()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BINInfoTheme {
-        Greeting("Android")
     }
 }
